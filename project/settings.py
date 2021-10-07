aecd4221c46bd73d7ddb62260ff9635d298e49cc
devrajshetake <79364404+devrajshetake@users.noreply.github.com>
2021-10-07 16:48:52 +0530

    login modal pop up, created Drive model

--- project/settings.py
+++ project/settings.py
@@ -74,10 +74,19 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'bvsj5208',
+        'HOST': 'localhost',
     }
 }
 
