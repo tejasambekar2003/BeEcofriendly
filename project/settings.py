6dd5b7ecbf5306c30e9217b63ea52b40ac2a963e
kaushal <kaushal110602@gmail.com>
2021-10-09 17:02:13 +0530

    db change

--- project/settings.py
+++ project/settings.py
@@ -77,21 +77,21 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'bvsj5208',
+        'HOST': 'localhost',
     }
 }
-# DATABASES = {
-#     # 'default': {
-#     #     'ENGINE': 'django.db.backends.postgresql',
-#     #     'NAME': 'test',
-#     #     'USER': 'postgres',
-#     #     'PASSWORD': 'bvsj5208',
-#     #     'HOST': 'localhost',
-#     # }
-# }
 
 
 # Password validation
